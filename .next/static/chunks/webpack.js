85e9f3ce1b0a1a8ddde2a096f8177cfc28e9bd4e
Vernon <wmvernon@proton.me>
2022-10-19 13:07:16 -0400

    edited NHASH project page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6bd8e7402de64a7a"; }
+/******/ 		__webpack_require__.h = function() { return "60d0fb476e1c78da"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/nhash.jsx
+++ pages/nhash.jsx
@@ -4,71 +4,78 @@ import NHASH from '../public/assets/projects/NHASH.png';
 import { RiRadioButtonFill } from 'react-icons/ri';
 import Link from 'next/link';
 
-const netflix = () => {
+const nhash = () => {
   return (
-    <div className='w-full'>
-      <div className='w-screen h-[50vh] relative'>
-        <div className='absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10' />
-        <Image
-          className='absolute z-1'
-          layout='fill'
-          objectFit='cover'
-          src={NHASH}
-          alt='/'
-        />
-        <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-          <h2 className='py-2'>NHASH App</h2>
-          <h3>React JS / Tailwind / Firebase</h3>
-        </div>
-      </div>
+		<div className='w-full'>
+			<div className='w-screen h-[50vh] relative'>
+				<div className='absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10' />
+				<Image
+					className='absolute z-1'
+					layout='fill'
+					objectFit='cover'
+					src={NHASH}
+					alt='/'
+				/>
+				<div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
+					<h2 className='py-2'>NHASH App</h2>
+					<h3>React JS / Tailwind / APIs / Data</h3>
+				</div>
+			</div>
 
-      <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8'>
-        <div className='col-span-4'>
-          <p>Project</p>
-          <h2>Overview</h2>
-          <p>
-          Test Text
-          </p>
-          <a
-            href='https://github.com/fireclint/netflix-react-tailwind'
-            target='_blank'
-            rel='noreferrer'
-          >
-            <button className='px-8 py-2 mt-4 mr-8'>Code</button>
-          </a>
-          <a
-            href='https://fireclint.github.io/netflix-react-tailwind/'
-            target='_blank'
-            rel='noreferrer'
-          >
-            <button className='px-8 py-2 mt-4'>Demo</button>
-          </a>
-        </div>
-        <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
-          <div className='p-2'>
-            <p className='text-center font-bold pb-2'>Technologies</p>
-            <div className='grid grid-cols-3 md:grid-cols-1'>
-              <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> React
-              </p>
-              <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Tailwind
-              </p>
-              <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Javascript
-              </p>
-              <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Firebase
-              </p>
-            </div>
-          </div>
-        </div>
-        <Link href='/#projects'>
-          <p className='underline cursor-pointer'>Back</p>
-        </Link>
-      </div>
-    </div>
+			<div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8'>
+				<div className='col-span-4'>
+					<p>Project</p>
+					<h2>Overview</h2>
+					<br />
+					<p>
+						During my Internship i was tasked with building
+						something intuitive and helpful and the CEO asked for me
+						to build some BitCoin Miners. He also requested a way
+						that he could have a web app open on his monitor so we
+						can see live feedback of what the miners are doing.
+					</p>
+					<br />
+					<a
+						href='https://github.com/Vern111/'
+						target='_blank'
+						rel='noreferrer'>
+						<button className='px-8 py-2 mt-4 mr-8'>Code</button>
+					</a>
+					<a
+						href='https://github.com/Vern111/'
+						target='_blank'
+						rel='noreferrer'>
+						<button className='px-8 py-2 mt-4'>Demo</button>
+					</a>
+				</div>
+				<div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
+					<div className='p-2'>
+						<p className='text-center font-bold pb-2'>
+							Technologies
+						</p>
+						<div className='grid grid-cols-3 md:grid-cols-1'>
+							<p className='text-gray-600 py-2 flex items-center'>
+								<RiRadioButtonFill className='pr-1' /> React
+							</p>
+							<p className='text-gray-600 py-2 flex items-center'>
+								<RiRadioButtonFill className='pr-1' /> Tailwind
+							</p>
+							<p className='text-gray-600 py-2 flex items-center'>
+								<RiRadioButtonFill className='pr-1' />{' '}
+								Javascript
+							</p>
+							<p className='text-gray-600 py-2 flex items-center'>
+								<RiRadioButtonFill className='pr-1' /> APIs
+							</p>
+						</div>
+					</div>
+				</div>
+				<Link href='/#projects'>
+					<p className='underline cursor-pointer'>Back</p>
+				</Link>
+			</div>
+		</div>
   );
 };
 
-export default netflix;
+export default nhash;
